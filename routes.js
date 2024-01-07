0d8c8c22912dc02b3f9c98669c8d905b2d797586
GuyPaikowsky <GuyPaikowsky@users.noreply.github.com>
2024-01-07 18:56:52 +0200

    Remove unused files. Remove unused routes.

--- routes.js
+++ routes.js
@@ -1,13 +1,8 @@
-import HomePage from './views/HomePage.js'
 import AboutUs from './views/AboutUs.js'
 
 import NoteIndex from './apps/keep/pages/NoteIndex.js'
 import NoteDetails from './apps/keep/cmps/NoteDetails.js'
 import NoteEdit from './apps/keep/pages/NoteIndex.js'
-//
-// import EmailIndex from './apps/mail/pages/EmailIndex.js'
-// import EmailDetails from './apps/mail/pages/EmailDetails.js'
-// import EmailList from './apps/mail/cmps/EmailList.js'
 
 const {createRouter, createWebHashHistory} = VueRouter
 
@@ -16,7 +11,7 @@ const routerOptions = {
 	routes: [
 		{
 			path: '/',
-			component: HomePage,
+			component: NoteIndex,
 		},
 		{
 			path: '/about',
@@ -33,49 +28,7 @@ const routerOptions = {
 		{
 			path: '/note/edit/:noteId?',
 			component: NoteEdit
-		},
-		// {
-		// 	path: '/email',
-		// 	component: EmailIndex,
-		// 	children: [
-		// 		{
-		// 			path: '',
-		// 			name: 'email list',
-		// 			component: EmailList
-
-		// 		},
-		// 		{
-		// 			path: ':emailId',
-		// 			name: 'email details',
-		// 			component: EmailDetails,
-		// 		},
-		// 		{
-		// 			path: 'trash',
-		// 			name: 'email trash',
-		// 			component: EmailList,
-		// 		},
-		// 		{
-		// 			path: 'draft',
-		// 			name: 'email draft',
-		// 			component: EmailList,
-		// 		},
-		// 		{
-		// 			path: 'sent',
-		// 			name: 'email sent',
-		// 			component: EmailList,
-		// 		},
-		// 		{
-		// 			path: 'important',
-		// 			name: 'email important',
-		// 			component: EmailList,
-		// 		},
-		// 		{
-		// 			path: 'starred',
-		// 			name: 'email important',
-		// 			component: EmailList,
-		// 		},
-		// 	]
-		// },
+		}
 	],
 }
 export const router = createRouter(routerOptions)
